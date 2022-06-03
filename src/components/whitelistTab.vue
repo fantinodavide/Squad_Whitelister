ad11ab88035c54bcdfee4c7517d06b06078094a6
Fantino Davide <fantinodavide02@gmail.com>
2022-06-03 13:54:35 +0200

    listing whitelist players

--- src/components/whitelistTab.vue
+++ src/components/whitelistTab.vue
@@ -4,12 +4,15 @@
 <script lang="ts">
 import { anyTypeAnnotation } from '@babel/types';
 import { stringifyStyle } from '@vue/shared';
+import $ from 'jquery';
+import whitelistUserCard from './whitelistUserCard.vue';
 
 export default {
 	data() {
 		return {
 			whitelist_clans: [] as Array<any>,
-			sel_clan: {} as any
+			sel_clan: {} as any,
+			wl_players: [] as Array<any>
 		}
 	},
 	methods: {
@@ -17,23 +20,46 @@ export default {
 			fetch("/api/whitelist/read/getAllClans").then(res => res.json()).then(dt => {
 				this.whitelist_clans = dt;
 				this.sel_clan = this.whitelist_clans[0]._id;
+				this.getClanWhitelist();
 			});
+		},
+		selectClanChanged: function (e: any) {
+			this.sel_clan = e.target.value;
+			this.getClanWhitelist();
+		},
+		getClanWhitelist() {
+			$.ajax({
+				url: "/api/whitelist/read/getAll",
+				type: "get",
+				data: { sel_clan_id: this.sel_clan },
+				dataType: "json",
+				contentType: 'application/json',
+				success: (dt) => {
+					console.log("Whitelist player", dt)
+					this.wl_players = dt;
+				},
+				error: (err) => {
+					console.error(err)
+				}
+			})
 		},
 		log: console.log
 	},
 	created() {
 		this.getWhitelistTabClans()
-	}
+	},
+	components: { whitelistUserCard }
 }
 </script>
 
 <template>
-	<select name="clan_selector" ref="clan_selector" :disabled="whitelist_clans.length == 1" @change="(v:any)=>{sel_clan = v.target.value}">
+	<select name="clan_selector" ref="clan_selector" :disabled="whitelist_clans.length == 1"
+		@change="selectClanChanged">
 		<option v-for="c of whitelist_clans" :value="c._id">{{ c.full_name }}
 		</option>
 	</select>
 	<button class="addHorizontal" @click="$emit('addNewWhitelistUser', sel_clan)"></button>
-
+	<whitelistUserCard v-for="w of wl_players" :wl_data="w" />
 </template>
 
 <style scoped>
---
+++ src/components/whitelistUserCard.vue
@@ -0,0 +1,152 @@
+<script setup lang="ts">
+import { assertExpressionStatement } from "@babel/types";
+import $ from 'jquery';
+import MarqueeText from 'vue-marquee-text-component'
+</script>
+
+<script lang="ts">
+export default {
+
+	data() {
+		return {
+			hoverMenuVisible: false,
+			hoverMenuLeft: 0
+		}
+	},
+	props: {
+		wl_data: {
+			required: true,
+			type: Object
+		}
+	},
+	methods: {
+		deleteRecord: function (successCB: any) {
+			console.log("Removing", this.$props.wl_data)
+			$.ajax({
+				url: "/api/gameGroups/write/remove",
+				type: "post",
+				data: this.$props.wl_data,
+				dataType: "json",
+				success: (dt) => {
+					console.log(dt);
+					successCB();
+					//location.reload();
+				}
+			})
+		},
+		updateHoverMenuLeft: function (e: any) {
+			this.hoverMenuLeft = e.target.scrollLeft
+		}
+	}
+}
+</script>
+
+<template>
+	<div class="gameGroupCard">
+		<div class="groupName tag">{{ wl_data.username }}
+		</div>
+		<span class="dot" :class="{ fill: wl_data.approved }"></span>
+		<!--<div class="clanTag"></div>-->
+		<div class="mainGroupContainer" @scroll="updateHoverMenuLeft">
+			<!-- <marquee-text :duration="10" :paused="false"></marquee-text> -->
+			<span class="tag">{{ wl_data.steamid64 }}</span>
+			<!-- <span class="steamid64">{{ wl_data.steamid64 }}</span> -->
+			<div class="hoverMenu" :class="{ vis: hoverMenuVisible }">
+				<button @click="$emit('confirm', { wl_data: wl_data, callback: deleteRecord })">Delete</button>
+				<button @click="$emit('edit', { wl_data: wl_data })">Edit</button>
+			</div>
+		</div>
+		<!-- <div class="btnContainer"><button @click="hoverMenuVisible = !hoverMenuVisible">Edit</button></div> -->
+	</div>
+</template>
+
+<style scoped>
+.hoverMenu {
+	position: absolute;
+	bottom: -100%;
+	left: v-bind(hoverMenuLeft + "px");
+	width: 100%;
+	height: calc(100% - 5px);
+	opacity: 1;
+	padding: 5px;
+	display: flex;
+	flex-direction: row;
+	flex-wrap: nowrap;
+	align-items: stretch;
+	transition: all 150ms ease-in-out;
+	background: #222;
+	overflow: auto;
+	border-top-left-radius: 15px;
+	border-top-right-radius: 15px;
+}
+
+.dot {
+	width: 20px;
+	height: 20px;
+	border-radius: 20px;
+	border: 3px solid var(--accent-color);
+	background: #0000;
+	margin: 10px;
+}
+
+.dot.fill {
+	background: var(--accent-color);
+}
+
+.groupName.tag {
+	margin-right: 0px;
+}
+
+.gameGroupCard:hover .hoverMenu,
+.hoverMenu.vis {
+	bottom: 0;
+}
+
+.gameGroupCard:hover .hoverMenu {
+	opacity: 1;
+	pointer-events: all;
+}
+
+.hoverMenu button {
+	border: 5px solid #fff2;
+	border-radius: 10px;
+	margin: 5px;
+	flex-grow: 1;
+	background: #0000;
+	color: #fff2;
+	transition: all 100ms ease-in-out;
+	max-width: 250px;
+}
+
+.hoverMenu button:hover {
+	color: var(--accent-color);
+	border-color: var(--accent-color);
+}
+
+.gameGroupCard {
+	border: none;
+	background: #2f2f2f;
+}
+
+
+.mainGroupContainer {
+	position: relative;
+	flex-grow: 1;
+	height: 100%;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	overflow: hidden;
+	width: 0px;
+	overflow-x: auto;
+	margin-right: 10px;
+}
+
+.groupName {
+	margin: 15px;
+	padding: 3px 10px;
+	background: var(--accent-color);
+	color: #2a2a2a;
+	border-radius: 13px;
+}
+</style>
