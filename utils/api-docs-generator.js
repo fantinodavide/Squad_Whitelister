c64800bdcc73a440a189ee5ac4eccb32de42b58f
Davide Fantino <fantinodavide02@gmail.com>
2026-02-10 08:53:39 +0000

    fix: swagger-autogen compat with mongo-sanitizer

--- utils/api-docs-generator.js
+++ utils/api-docs-generator.js
@@ -1,9 +1,24 @@
 const fs = require('fs');
-const swaggerAutogen = require('swagger-autogen')({ writeOutputFile: false })
+const path = require('path');
+const swaggerAutogen = require('swagger-autogen')({ writeOutputFile: false });
 
 main();
 
 async function main() {
-    apiDocsJson = await swaggerAutogen('./swagger-output.json', [ 'server.js' ]);
-    fs.writeFileSync('docs/api/docs.json', JSON.stringify(apiDocsJson))
+	const serverPath = path.resolve(__dirname, '..', 'server.js');
+	const tmpPath = path.resolve(__dirname, '..', '.server.swagger.tmp.js');
+
+	let src = fs.readFileSync(serverPath, 'utf8');
+	src = src
+		.replace(/req\.sanitizedBody/g, 'req.body')
+		.replace(/req\.sanitizedQuery/g, 'req.query')
+		.replace(/req\.sanitizedParams/g, 'req.params');
+	fs.writeFileSync(tmpPath, src);
+
+	try {
+		const apiDocsJson = await swaggerAutogen('./swagger-output.json', [tmpPath]);
+		fs.writeFileSync('docs/api/docs.json', JSON.stringify(apiDocsJson));
+	} finally {
+		fs.unlinkSync(tmpPath);
+	}
 }
